7914fbb0664a928bae8b1028abc2e5bd0d5f3968
Russell Chapman <chpmnrssll@gmail.com>
2013-11-07 16:54:00 -0700

    update user views

--- js/admin/users/updateView.js
+++ js/admin/users/updateView.js
@@ -1,20 +1,21 @@
-define([ "jquery", "underscore", "backbone", "marionette", "text!admin/categories/updateTemplate.html" ], function($, _, Backbone, Marionette, Template) {
+define([ "jquery", "underscore", "backbone", "marionette", "text!admin/users/updateTemplate.html" ], function($, _, Backbone, Marionette, Template) {
 	return Backbone.Marionette.CompositeView.extend({
 		template: _.template(Template),
 		className: "container",
 		events: {
 			"click #userSave": function (event) {
+				console.log($("userEmail").val());
 				this.model.save({
 					name: $("#userName").val(),
-					email: $("userEmail").val()
+					email: $("#userEmail").val()
 				}, {
 					success: function (model, response, options) {
-						window.App.router.navigate("admin/categories", { trigger: true });
+						window.App.router.navigate("admin/users", { trigger: true });
 					}
 				});
 			},
 			"click #userCancel": function (event) {
-				window.App.router.navigate("admin/categories", { trigger: true });
+				window.App.router.navigate("admin/users", { trigger: true });
 			}
 		}
 	});
